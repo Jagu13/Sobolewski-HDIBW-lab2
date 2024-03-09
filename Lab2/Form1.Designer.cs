7db38ac070ca43661eddf2a41b4631386a79775d
Jagoda <jagoda.kwiatkowska39@gmail.com>
2024-03-09 18:54:18 +0100

    Initialize projekt

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(939, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(1, 182);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(143, 264);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(150, 182);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(155, 264);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(311, 182);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(159, 264);
+            this.listBox4.TabIndex = 3;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(476, 182);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(154, 264);
+            this.listBox5.TabIndex = 4;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(636, 182);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(140, 264);
+            this.listBox6.TabIndex = 5;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(782, 182);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(158, 264);
+            this.listBox7.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(939, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(375, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "E:\\DominikBorkowski\\HurtownieBazDanych\\dane\\db\\ZALog2003.10.01.txt";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Czytaj plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ilość linii: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OdczytajPlik();
+        }
+
+        void OdczytajPlik()
+        {
+            string fileName = @"E:\DominikBorkowski\HurtownieBazDanych\dane\db\ZALog2003.10.01.txt";
+            try
+            {
+                // Create a StreamReader
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    // Read line by line
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(line);
+                        listBox1.Items.Add(line);
+                        if (line.Contains(","))
+                        {
+                            int countPrzecinki = line.Count(f => f == ',');
+                            if (countPrzecinki == 5)
+                            {
+
+                            }
+                        }
+                    }
+                    label1.Text = "ilość linii: " + counter;
+                    label1.Refresh();
+                    Console.WriteLine(counter);
+                }
+            }
+            catch (Exception exp)
+            {
+                //Console.WriteLine(exp.Message);
+            }
+
+        }
+
+        string OdczytajElement(ref string line)
+        {
+            string[] splitData = line.Split(',');
+            String typ = splitData[0];
+            String data = splitData[1];
+            String czas = splitData[2];
+            String adresWe = splitData[3];
+            String adresWy = splitData[4];
+            String protokol = splitData[5];
+
+            listBox2.Items.Add(typ);
+            listBox3.Items.Add(data);
+            listBox4.Items.Add(czas);
+            listBox5.Items.Add(adresWe);
+            listBox6.Items.Add(adresWy);
+            listBox7.Items.Add(protokol);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
